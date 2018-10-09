89c5deeff46f4e904a696f685eb93a33d9dacb7e
wsetiono <vincentius.william.setiono@gmail.com>
2018-10-09 19:48:44 +0700

    Commit CategoryController.cs

---
+++ OctopusCodes.App/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OctopusCodesShoppingCartPaypal.Core.Services.Categories;
+using OctopusCodesShoppingCartPaypal.Core.Services.Settings;
+using OctopusCodesShoppingCartPaypal.Core.Services.Users;
+
+namespace OctopusCodes.App.Controllers
+{
+    public class CategoryController : BaseController
+    {
+        //
+        // GET: /Category/
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService, IUserService userService, ISettingService settingService) : base(userService, settingService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GetCategories()
+        {
+            return Json(new { result = _categoryService.GetAll().ToList() });
+        }
+
+        public ActionResult GetMenu()
+        {
+            return PartialView("_CategoryMenu", _categoryService.GetAll().OrderBy(a => a.Ancestor).ToList());
+        }
+    }
+}
