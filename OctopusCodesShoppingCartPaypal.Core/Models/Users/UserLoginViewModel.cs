56e4d1d4f67e5c004f1f7f7fa4f7799dd007a051
wsetiono <vincentius.william.setiono@gmail.com>
2018-10-09 19:41:43 +0700

    Commit AccountController.cs

---
+++ OctopusCodesShoppingCartPaypal.Core/Models/Users/UserLoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctopusCodesShoppingCartPaypal.Core.Models.Users
+{
+    public class UserLoginViewModel : IUser
+    {
+        public UserLoginViewModel()
+        {
+            //Id = Guid.NewGuid().ToString();
+        }
+        public string Id { get; private set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public bool Remember { get; set; }
+    }
+}
