a3da653a4f7cf64b62947f8fcbe709edb82252e8
wsetiono <vincentius.william.setiono@gmail.com>
2018-10-09 19:57:06 +0700

    Commit OrdersController.cs

---
+++ OctopusCodesShoppingCartPaypal.Core/Models/Orders/OrdersViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctopusCodesShoppingCartPaypal.Core.Models.Orders
+{
+    public class OrdersViewModel
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int Status { get; set; }
+        public string StrStatus { get; set; }
+        public Double Total { get; set; }
+    }
+
+    public class OrdersUserViewModel
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int Status { get; set; }
+        public string StrStatus { get; set; }
+        public int Amount { get; set; }
+        public Double Total { get; set; }
+    }
+}
